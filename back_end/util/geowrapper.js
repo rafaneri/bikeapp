29f83052e5981e84875b2b49d1d3890ceef3165c
Rafael da Mata Neri <rafael.neri@gmail.com>
2016-05-15 17:10:48 -0300

    Add waypoint name to result

--- back_end/util/geowrapper.js
+++ back_end/util/geowrapper.js
@@ -3,10 +3,10 @@ function geoWrapper(o, p1, p2, d) {
         "type": "LineString",
         "properties": {
             "waypoints":[
-                {"coordinates":[o.longitude, o.latitude]},
-                {"coordinates":[p1.geometry.coordinates[0], p1.geometry.coordinates[1]]},
-                {"coordinates":[p2.geometry.coordinates[0], p2.geometry.coordinates[1]]},
-                {"coordinates":[d.longitude, d.latitude]}
+                {"coordinates":[o.longitude, o.latitude], "name": o.streetName},
+                {"coordinates":[p1.geometry.coordinates[0], p1.geometry.coordinates[1]], "name": p1.properties.name},
+                {"coordinates":[p2.geometry.coordinates[0], p2.geometry.coordinates[1]], "name": p2.properties.name},
+                {"coordinates":[d.longitude, d.latitude], "name": d.streetName}
             ]
         }
     };
